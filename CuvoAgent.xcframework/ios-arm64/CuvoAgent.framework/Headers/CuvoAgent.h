768e45545f37c451117101daeefd4d8f9542c051
Aravindhan <v_aravindhan.natarajan@circles.co>
2025-02-24 10:24:28 +0530

    New SDK Update with Version Increment

---
+++ CuvoAgent.xcframework/ios-arm64/CuvoAgent.framework/Headers/CuvoAgent.h
@@ -0,0 +1,17 @@
+//
+//  CuvoAgent.h
+//  CuvoAgent
+//
+//  Created by siva chandran on 04/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CuvoAgent.
+FOUNDATION_EXPORT double CuvoAgentVersionNumber;
+
+//! Project version string for CuvoAgent.
+FOUNDATION_EXPORT const unsigned char CuvoAgentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CuvoAgent/PublicHeader.h>
+
